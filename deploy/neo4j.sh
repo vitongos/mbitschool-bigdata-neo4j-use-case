db9c3ee43b85a7ce1a13a6d9ba20a6b23da3af4c
Victor <vitongos@gmail.com>
2015-06-05 07:16:00 +0200

    Sample data.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -7,8 +7,10 @@ rm -rf neo4j.tar.gz
 sudo mv neo4j-community-2.2.2/ /opt/neo4j/
 sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
 cd ~/use-case-src/
+sudo mkdir /opt/neo4j/data/graph.db/
 sudo mv data/apps.db.tar.gz /opt/neo4j/data/graph.db/
 cd /opt/neo4j/data/graph.db/
 sudo tar zxf apps.db.tar.gz
 sudo rm -rf apps.db.tar.gz
 sudo sed -i -r 's/dbms\.security\.auth_enabled=true/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j-server.properties
+neo4j start
