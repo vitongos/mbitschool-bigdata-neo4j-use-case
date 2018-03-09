54b9e9f3c41e8526b830a189106ba8b107444a9f
Victor Santiago <vitongos@users.noreply.github.com>
2018-03-09 11:24:15 +0100

    Update to storm 1.2.1

--- deploy/storm.sh
+++ deploy/storm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /opt
-wget http://apache.rediris.es/storm/apache-storm-1.1.0/apache-storm-1.1.0.tar.gz
-tar zxf apache-storm-1.1.0.tar.gz
-mv apache-storm-1.1.0 storm
-rm apache-storm-1.1.0.tar.gz
+wget http://apache.uvigo.es/storm/apache-storm-1.2.1/apache-storm-1.2.1.tar.gz
+tar zxf apache-storm-1.2.1.tar.gz
+mv apache-storm-1.2.1 storm
+rm apache-storm-1.2.1.tar.gz
 ln -s /opt/storm/bin/storm /usr/bin/storm
 chown centos:centos /opt/storm -R
