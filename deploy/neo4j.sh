5c10f140ef5a4f9fb7896af381e0234d890c724a
Victor Santiago <vitongos@users.noreply.github.com>
2019-03-01 09:52:26 +0100

    Update neo4j.sh

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
 
-cd /opt/
-wget https://neo4j.com/artifact.php?name=neo4j-community-3.2.0-unix.tar.gz
-mv "artifact.php?name=neo4j-community-3.2.0-unix.tar.gz" neo4j-community-3.2.0-unix.tar.gz
-tar xzf neo4j-community-3.2.0-unix.tar.gz
-adduser neo4j -s /sbin/nologin
-chown neo4j:centos /opt/neo4j-community-3.2.0 -R
-rm neo4j-community-3.2.0-unix.tar.gz
+cd /tmp
+wget http://debian.neo4j.org/neotechnology.gpg.key
+rpm --import neotechnology.gpg.key
 
-cat << EOF > /lib/systemd/system/neo4j.service
-[Unit] 
-Description=Neo4j Management Service
-
-[Service]
-Type=forking
-User=neo4j
-ExecStart=/opt/neo4j-community-3.2.0/bin/neo4j start
-ExecStop=/opt/neo4j-community-3.2.0/bin/neo4j stop
-ExecReload=/opt/neo4j-community-3.2.0/bin/neo4j restart
-RemainAfterExit=no
-Restart=on-failure
-PIDFile=/opt/neo4j-community-3.2.0/run/neo4j.pid
-LimitNOFILE=60000
-TimeoutSec=600
-
-[Install]
-WantedBy=multi-user.target
+cat <<EOF>  /etc/yum.repos.d/neo4j.repo
+[neo4j]
+name=Neo4j Yum Repo
+baseurl=http://yum.neo4j.org/stable
+enabled=1
+gpgcheck=1
 EOF
 
-sed -i "s/#dbms.security.auth_enabled=false/dbms.security.auth_enabled=false/" /opt/neo4j-community-3.2.0/conf/neo4j.conf
-sed -i "s/#dbms.shell.enabled=true/dbms.shell.enabled=true/" /opt/neo4j-community-3.2.0/conf/neo4j.conf
+yum install -y neo4j
+
+sed -i -r 's/#dbms\.security\.auth_enabled=false/dbms\.security\.auth_enabled=false/' /etc/neo4j/neo4j.conf
+sed -i -r 's/#dbms\.allow_upgrade=true/dbms\.allow_upgrade=true/' /etc/neo4j/neo4j.conf
+
 systemctl daemon-reload
-systemctl enable neo4j.service
-systemctl start neo4j.service
-sleep 5
+
+service neo4j restart
 
 cd ~/use-case-src/
-/opt/neo4j-community-3.2.0/bin/neo4j-shell -file data/insert.cyp
+cypher-shell < data/insert.cyp
